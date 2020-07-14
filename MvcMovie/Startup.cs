e026d0e99ffde3365198685386f5bd86a12b2a0e
thanhd <15520798@gm.uit.edu.vn>
2020-07-14 15:22:00 +0700

    generate MoviesController from View using EF

--- MvcMovie/Startup.cs
+++ MvcMovie/Startup.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MvcMovie.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace MvcMovie
 {
@@ -14,9 +17,17 @@ namespace MvcMovie
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; private set; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration.GetConnectionString("MvcMovieContext");
             services.AddControllersWithViews();
+            services.AddDbContext<MvcMovieContext>(options =>
+            options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
