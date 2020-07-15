906742c8e22f9bfda4a5bf7a458880c28fdf73d5
thanhd <15520798@gm.uit.edu.vn>
2020-07-15 11:39:57 +0700

    Update control handle and add font awesome in libma

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -15,10 +15,12 @@ namespace MvcMovie.Models
 
         [Display(Name ="Ngày p.hành")]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         [Display(Name = "Thể loại")]
         public string Genre { get; set; }
-        [Column(TypeName = "decimal(18, 2)")]
+        [Column(TypeName = "decimal(18,0)")]
+        [DisplayFormat(DataFormatString = "{0:#}")]
         [Display(Name = "Giá vé")]
         public decimal Price { get; set; }
     }
