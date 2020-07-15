4fbc8d1aa8afc5f80d59e3d9db8195839d1200af
thanhd <15520798@gm.uit.edu.vn>
2020-07-15 16:18:32 +0700

    config web-api using route in launchSettings

---
+++ MvcMovieApi/Controllers/MoviesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MvcMovieApi.Models;
+
+namespace MvcMovieApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly MovieContext _context;
+
+        public MoviesController(MovieContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Movies
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MovieItem>>> GetMovieItems()
+        {
+            return await _context.MovieItems.ToListAsync();
+        }
+
+        // GET: api/Movies/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MovieItem>> GetMovieItem(int id)
+        {
+            var movieItem = await _context.MovieItems.FindAsync(id);
+
+            if (movieItem == null)
+            {
+                return NotFound();
+            }
+
+            return movieItem;
+        }
+
+        // PUT: api/Movies/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMovieItem(int id, MovieItem movieItem)
+        {
+            if (id != movieItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(movieItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Movies
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<MovieItem>> PostMovieItem(MovieItem movieItem)
+        {
+            _context.MovieItems.Add(movieItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction( nameof(GetMovieItem), new { id = movieItem.Id }, movieItem);
+        }
+
+        // DELETE: api/Movies/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<MovieItem>> DeleteMovieItem(int id)
+        {
+            var movieItem = await _context.MovieItems.FindAsync(id);
+            if (movieItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.MovieItems.Remove(movieItem);
+            await _context.SaveChangesAsync();
+
+            return movieItem;
+        }
+
+        private bool MovieItemExists(int id)
+        {
+            return _context.MovieItems.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ MvcMovieApi/Models/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovieApi.Models
+{
+    public class MovieContext: DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<MovieItem> MovieItems { get; set; }
+    }
+}
